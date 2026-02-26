f28cd9e8e1b15b2b5e046f44fbc71169b9cb3dba
Yashas <yashas.yadav@cloudsufi.com>
2026-02-26 11:43:40 +0530

    Modified the existing packages

--- tms-backend/src/main/java/cloudsufi/nextgen/tms/dto/request/package-info.java
+++
@@ -1,3 +0,0 @@
-// Contains request DTO classes used as inbound API payloads.
-
-package cloudsufi.nextgen.tms.dto.request;
--- tms-backend/src/main/java/cloudsufi/nextgen/tms/dto/response/package-info.java
+++
@@ -1,3 +0,0 @@
-// Contains response DTO classes returned in API responses.
-
-package cloudsufi.nextgen.tms.dto.response;
