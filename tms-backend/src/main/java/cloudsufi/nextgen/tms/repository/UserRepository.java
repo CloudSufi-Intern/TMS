478ed564386199ceb09f752e010e0b336778fddb
pgupta771 <priyanshugupta771@gmail.com>
2026-03-09 15:52:19 +0530

    chore:remove intellij .idea folder from repo

--- tms-backend/src/main/java/cloudsufi/nextgen/tms/repository/UserRepository.java
+++ tms-backend/src/main/java/cloudsufi/nextgen/tms/repository/UserRepository.java
@@ -1,6 +1,11 @@
 package cloudsufi.nextgen.tms.repository;
 import java.util.Optional;
 
+/**
+ * Repository interface for accessing User data from databse
+ *
+ * author : priyanshu gupta
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User>findByEmail(String email);
