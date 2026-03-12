b9497ba580249303fe74525eb797d88852d8e302
vishwasvaidya-cloudsufi <vishwas.vaidya@cloudsufi.com>
2026-03-12 12:04:25 +0530

    [#12] feat: implement user search API with top-10 suggestions

--- tms-backend/src/main/java/cloudsufi/nextgen/tms/service/UserService.java
+++ tms-backend/src/main/java/cloudsufi/nextgen/tms/service/UserService.java
@@ -2,12 +2,16 @@ package cloudsufi.nextgen.tms.service;
 
 import cloudsufi.nextgen.tms.dto.GetUserResponse;
 import cloudsufi.nextgen.tms.dto.UserRequestDTO;
+import cloudsufi.nextgen.tms.dto.UserSuggestionDTO;
 import cloudsufi.nextgen.tms.entity.UserEntity;
 import cloudsufi.nextgen.tms.exception.BadRequestException;
 import cloudsufi.nextgen.tms.exception.ResourceNotFoundException;
 import cloudsufi.nextgen.tms.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -115,4 +119,28 @@ public class UserService {
                 .role(userEntity.getRole())
                 .build();
     }
+
+    /**
+     * Searches users whose usernames start with the given keyword.
+     *
+     * This method supports pagination for efficient database querying.
+     *
+     * @param username The username prefix used for searching users.
+     * @param page     The page number to retrieve (0-based index).
+     * @param size     The number of records to return per page.
+     *
+     * @return A paginated {@link Page} containing {@link UserSuggestionDTO}
+     * objects representing matching users.
+     * @author vishwasvaidya
+     */
+
+
+    public Page<UserSuggestionDTO> searchUsers(String username, int page, int size) {
+        if (username == null || username.trim().length() < 2) {
+            throw new IllegalArgumentException("Username must contain at least 2 characters");
+        }
+        Pageable pageable = PageRequest.of(page, size);
+        return userRepository.searchUsers(username, pageable);
+    }
+
 }
