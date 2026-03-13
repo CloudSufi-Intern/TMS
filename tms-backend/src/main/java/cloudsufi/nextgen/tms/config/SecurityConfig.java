355fef01e5c3d57ccb46da3a0f222b708405860e
shubhanshu <shubhanshu71@gmail.com>
2026-03-13 11:52:25 +0530

    added customizer in security file

--- tms-backend/src/main/java/cloudsufi/nextgen/tms/config/SecurityConfig.java
+++ tms-backend/src/main/java/cloudsufi/nextgen/tms/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
@@ -42,6 +43,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http
+                // Enable CORS support in Spring Security
+                // This tells Spring Security to use the CorsFilter defined in CorsConfig
+                .cors(Customizer.withDefaults())
+
                 .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
                 .formLogin(form->form.disable())
                 .httpBasic(basic->basic.disable())
